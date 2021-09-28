c7b258271cc6cee019959343a7452f173f3406af
임빈영 <qlsdud960604@gmail.com>
2021-09-28 19:45:00 +0900

    Add: Footer.js 파일 추가

--- delivery-together-frontend/src/App.js
+++ delivery-together-frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Route } from 'react-router';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Map from './pages/Map';
 import MatchingForm from './pages/MatchingForm';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Container>
+    <div style={{ height: '100vh' }}>
+      <Header style={{ height: '10vh' }} />
+      <Container style={{ height: '90vh' }}>
         <Route path="/" exact={true} component={Map} />
         <Route path="/matchingForm" exact={true} component={MatchingForm} />
       </Container>
+      <Footer style={{ height: '10vh' }} />
     </div>
   );
 }
---
+++ delivery-together-frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="sticky-footer" class="py-3 bg-dark text-white-50">
+      <div class="container text-center">
+        <small>Copyright &copy; Laptop Boys</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
