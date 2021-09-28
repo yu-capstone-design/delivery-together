58d9e56e531c13a98451c47ef7ec255097d922e3
임빈영 <qlsdud960604@gmail.com>
2021-09-28 15:17:23 +0900

    프로젝트 생성 및 환경설정

---
+++ delivery-together-backend/src/test/java/com/example/deliverytogetherbackend/DeliveryTogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliverytogetherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTogetherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
