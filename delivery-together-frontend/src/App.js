fdefa31460d088f4d9638f668fb31c3638a27156
임빈영 <qlsdud960604@gmail.com>
2021-09-28 19:23:52 +0900

    Add: MatchingForm.js 파일 추가

--- delivery-together-frontend/src/App.js
+++ delivery-together-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import { Route } from 'react-router';
 import Header from './components/Header';
 import Map from './pages/Map';
+import MatchingForm from './pages/MatchingForm';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Container>
         <Route path="/" exact={true} component={Map} />
+        <Route path="/matchingForm" exact={true} component={MatchingForm} />
       </Container>
     </div>
   );
---
+++ delivery-together-frontend/src/pages/MatchingForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MatchingForm = () => {
+  return <div>매칭 등록 페이지입니다.</div>;
+};
+
+export default MatchingForm;
