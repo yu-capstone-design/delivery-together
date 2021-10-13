d04cf2420f0d4e0214ee5acc46b1dfdcfa7bfcce
임빈영 <qlsdud960604@gmail.com>
2021-10-13 15:24:05 +0900

    Add: SecurityConfig.java 파일 추가

--- delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/config/SecurityConfig.java
+++ delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/config/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.example.deliverytogetherbackend.config;public class SecurityConfig {
+package com.example.deliverytogetherbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity   // 시큐리티 필터를 스프링 필터 체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable();
+    }
 }
