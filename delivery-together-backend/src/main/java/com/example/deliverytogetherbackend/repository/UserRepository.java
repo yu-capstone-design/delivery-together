68d415c6c6541ee92fa967ddcc05d0e4b2647388
임빈영 <qlsdud960604@gmail.com>
2021-10-13 15:24:33 +0900

    Add: UserRepository.java 파일 추가

--- delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/repository/UserRepository.java
+++ delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/repository/UserRepository.java
@@ -1,2 +1,30 @@
-package com.example.deliverytogetherbackend.repository;public class UserRepository {
+package com.example.deliverytogetherbackend.repository;
+
+import com.example.deliverytogetherbackend.domain.User;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    public static final String COLLECTION_NAME = "users";
+
+
+    public String insertUser(User user) throws Exception {
+        Firestore firestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = firestore.collection(COLLECTION_NAME).document(user.getUsername());
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot documentSnapshot = apiFuture.get();
+
+        if (documentSnapshot.exists()) {
+            return "중복된 계정의 회원이 존재합니다.";
+        } else {
+            documentReference.set(user);
+            return "회원가입에 성공하였습니다.";
+        }
+    }
 }
