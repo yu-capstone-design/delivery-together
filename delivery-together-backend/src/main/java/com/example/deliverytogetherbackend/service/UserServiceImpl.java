c89cc09684af493b61b0d13dc92e61379d29229d
임빈영 <qlsdud960604@gmail.com>
2021-10-13 15:25:22 +0900

    Add: UserServiceImpl.java 파일 추가

--- delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/service/UserServiceImpl.java
+++ delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/service/UserServiceImpl.java
@@ -1,2 +1,25 @@
-package com.example.deliverytogetherbackend.service;public class UserServiceImpl {
+package com.example.deliverytogetherbackend.service;
+
+import com.example.deliverytogetherbackend.domain.User;
+import com.example.deliverytogetherbackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    @Override
+    public String registerUser(User user) throws Exception {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+
+        return userRepository.insertUser(user);
+    }
 }
