42abef8e2c3ed27ee3df6a21354c597687a18e16
임빈영 <qlsdud960604@gmail.com>
2021-10-13 15:25:37 +0900

    Add: UserController.java 파일 추가

--- delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/controller/UserController.java
+++ delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/controller/UserController.java
@@ -1,2 +1,24 @@
-package com.example.deliverytogetherbackend.controller;public class UserController {
+package com.example.deliverytogetherbackend.controller;
+
+import com.example.deliverytogetherbackend.domain.User;
+import com.example.deliverytogetherbackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @CrossOrigin
+    @PostMapping("/join")
+    public ResponseEntity<?> joinUser(@RequestBody User user) throws Exception {
+        return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
+    }
 }
