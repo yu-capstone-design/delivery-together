8a5f35f541e90af5124b8752f6d217da3d5573f3
임빈영 <qlsdud960604@gmail.com>
2021-10-17 01:51:04 +0900

    Add: Home.js 파일 추가

---
+++ delivery-together-frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        height: '90%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1>Delivery Together에 오신것을 환영합니다.</h1>
+    </div>
+  );
+};
+
+export default Home;
