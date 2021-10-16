33ec01c6f5dc4b1d845060ea0ad46cadc15f6879
Smileman1 <ejrwnd1103@naver.com>
2021-10-17 05:28:24 +0900

    Add: Chat 디자인 추가

--- delivery-together-frontend/src/pages/chat/Chat.js
+++ delivery-together-frontend/src/pages/chat/Chat.js
@@ -1,39 +1,139 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-} from "@chatscope/chat-ui-kit-react";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { conversationChanged, newMessageAdded, conversationDeleted, conversationsRequested } from './store/action';
+import ConversationSearch from '../../components/conversation/conversationSearch/ConversationSearch';
+import NoConversations from '../../components/conversation/noCoversations/NoConversations';
+import ConversationList from '../../components/conversation/conversationList/ConversationList';
+import NewConversation from '../../components/conversation/newConversation/NewConversation';
+import ChatTitle from '../../components/chatTitle/ChatTitle';
+import MessageList from './message/MessageList';
+import ChatForm from '../../components/chatForm/ChatForm';
+import '../chat/style/Chat.css'
 
+const Chat = ({
+                conversations,
+                selectedConversation,
+                conversationChanged,
+                onMessageSubmitted,
+                onDeleteConversation,
+                loadConversations
+              }) => {
+  useEffect(() => {
+    loadConversations();
+  }, [loadConversations]);
 
-const Chat = (props) => {
+  const [textMessage, setTextMessage] = useState('');
+  const [markers, setMarkers] = useState([{example: []}]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/chat')
+      .then((res) => res.json())
+      .then((res) => {
+        setMarkers(res);
+      });
+
+  }, [markers]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    var abc = [...markers[0].example]
+    console.log("---------")
+    console.log(abc)
+
+    abc.push({
+      msg : textMessage,
+      sender: "김덕중"
+    })
+
+    var chat = {
+      msg : textMessage,
+      sender : '김덕중',
+      createdAt : '오늘',
+      roomNum : 11,
+      example : abc
+    }
+    console.log(JSON.stringify(chat))
+    fetch('http://localhost:8080/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify(chat),
+    })
+      .then((res) => {
+        console.log(res.text());
+
+        if (res.status === 201) {
+          return res;
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        if (res != null) {
+          alert('매칭 등록에 성공하였습니다.');
+        } else {
+          alert('매칭 등록에 실패하였습니다.');
+        }
+      });
+
+  }
+
+  let brr = markers[0].example.map((value, key) => <li key = {key}>{value.msg}</li>);
+
+  // let messageItems = markers[0].example.map((message, index) => {
+  //   return <Message
+  //     key={index}
+  //     isMyMessage={message.sender === "김덕중"}
+  //     message={message.msg} />;
+  // });
+
+  let conversationContent = (
+    <>
+      <NoConversations></NoConversations>
+    </>
+  );
+
+  if (conversations.length > 0) {
+    conversationContent = (
+      <>
+        <MessageList conversationId={selectedConversation.id} />
+      </>
+    );
+  }
+
   return (
-    <div style={{ height: '94vh' }}>
-      <div>
-        아아아
-      </div>
-      <div style={{ position: "relative", height: "700px" }}>
-        <MainContainer>
-          <ChatContainer>
-            <MessageList>
-              <Message
-                model={{
-                  message: "Hello my friend",
-                  sentTime: "just now",
-                  sender: "Joe",
-                }}
-              />
-
-            </MessageList>
-            <MessageInput placeholder="Type message here" />
-          </ChatContainer>
-        </MainContainer>
-      </div>
+    <div id="chat-container">
+      <ChatTitle
+        selectedConversation={selectedConversation}
+        onDeleteConversation={onDeleteConversation} />
+        <div className='abc'>
+          {conversationContent}
+        </div>
+      <ChatForm
+        selectedConversation={selectedConversation}
+        onMessageSubmitted={onMessageSubmitted} />
     </div>
   );
+}
+
+const mapStateToProps = state => {
+  return {
+    conversations: state.conversationState.conversations,
+    selectedConversation: state.conversationState.selectedConversation
+  };
 };
-export default Chat;
+
+const mapDispatchToProps = dispatch => ({
+  conversationChanged: conversationId => dispatch(conversationChanged(conversationId)),
+  onMessageSubmitted: messageText => { dispatch(newMessageAdded(messageText)); },
+  onDeleteConversation: () => { dispatch(conversationDeleted()); },
+  loadConversations: () => { dispatch(conversationsRequested())}
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Chat);
--- delivery-together-frontend/src/pages/chat/store/sagas/conversations.js
+++ delivery-together-frontend/src/pages/chat/store/sagas/conversations.js
@@ -16,85 +16,22 @@ const conversations = [
       {
         imageUrl: null,
         imageAlt: null,
-        messageText: 'Ok then',
+        messageText: 'ㅇㅋㅇㅋ',
+        createdAt: 'Apr 16',
+        isMyMessage: true
+      },
+      {
+        imageUrl: null,
+        imageAlt: null,
+        messageText: 'ㄴㄴㄴ',
         createdAt: 'Apr 16',
         isMyMessage: true
       }
     ]
-  },
-  {
-    id: '2',
-    imageUrl: require('../../images/profiles/kim.jpeg'),
-    imageAlt: 'Kim O\'Neil',
-    title: 'Kim O\'Neil',
-    createdAt: 'Oct 20',
-    latestMessageText: 'Ok fair enough. Well good talking to you.',
-    messages: []
-  },
-  {
-    id: '3',
-    imageUrl: require('../../images/profiles/john.jpeg'),
-    imageAlt: 'John Anderson',
-    title: 'John Anderson',
-    createdAt: '1 week ago',
-    latestMessageText: 'Yes I love how Python does that',
-    messages: []
-  },
-  {
-    id: '4',
-    imageUrl: require('../../images/profiles/ben.png'),
-    imageAlt: 'Ben Smith',
-    title: 'Ben Smith',
-    createdAt: '2:49 PM',
-    latestMessageText: 'Yeah Miami Heat are done',
-    messages: []
-  },
-  {
-    id: '5',
-    imageUrl: require('../../images/profiles/douglas.png'),
-    imageAlt: 'Douglas Johannasen',
-    title: 'Douglas Johannasen',
-    createdAt: '6:14 PM',
-    latestMessageText: 'No it does not',
-    messages: []
-  },
-  {
-    id: '6',
-    imageUrl: require('../../images/profiles/jacob.png'),
-    imageAlt: 'Jacob Manly',
-    title: 'Jacob Manly',
-    createdAt: '3 secs ago',
-    latestMessageText: 'Just be very careful doing that',
-    messages: []
-  },
-  {
-    id: '7',
-    imageUrl: require('../../images/profiles/stacey.jpeg'),
-    imageAlt: 'Stacey Wilson',
-    title: 'Stacey Wilson',
-    createdAt: '30 mins ago',
-    latestMessageText: 'Awesome!!! Congratulations!!!!',
-    messages: []
-  },
-  {
-    id: '8',
-    imageUrl: require('../../images/profiles/stan.jpeg'),
-    imageAlt: 'Stan George',
-    title: 'Stan George',
-    createdAt: '1 week ago',
-    latestMessageText: 'Good job',
-    messages: []
-  },
-  {
-    id: '9',
-    imageUrl: require('../../images/profiles/sarah.jpeg'),
-    imageAlt: 'Sarah Momes',
-    title: 'Sarah Momes',
-    createdAt: '1 year ago',
-    latestMessageText: 'Thank you. I appreciate that.',
-    messages: []
   }
 ];
+
+
 
 export const conversationsSaga = function*() {
   yield delay(1000);
