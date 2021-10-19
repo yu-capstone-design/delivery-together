956a868655f930983559144ce392dd446b057369
임빈영 <qlsdud960604@gmail.com>
2021-10-19 17:02:13 +0900

    Comment: 기능별 주석 추가

--- delivery-together-frontend/src/redux/actions.js
+++ delivery-together-frontend/src/redux/actions.js
@@ -1,3 +1,27 @@
+//==============================회원가입==============================
+/* 회원가입 요청에 대한 액션 */
+export const joinRequest = () => {
+  return {
+    type: 'JOIN_REQUEST',
+  };
+};
+
+/* 회원가입 성공에 대한 액션 */
+export const joinSuccess = () => {
+  return {
+    type: 'JOIN_SUCCESS',
+  };
+};
+
+/* 회원가입 실패에 대한 액션 */
+export const joinFailure = (error) => {
+  return {
+    type: 'JOIN_FAILURE',
+    payload: error,
+  };
+};
+
+//==============================로그인==============================
 /* 로그인 요청에 대한 액션 */
 export const loginRequest = () => {
   return {
@@ -21,6 +45,7 @@ export const loginFailure = (error) => {
   };
 };
 
+//==============================로그아웃==============================
 /* 로그아웃에 대한 액션 */
 export const logoutRequest = () => {
   return {
@@ -28,6 +53,7 @@ export const logoutRequest = () => {
   };
 };
 
+//==============================사용자 정보==============================
 /* 사용자 정보 요청에 대한 액션 */
 export const userDataRequest = (content) => {
   return {
