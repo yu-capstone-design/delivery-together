a918726c737574ba9513a7aa3d701e3a8abe5fee
임빈영 <qlsdud960604@gmail.com>
2021-10-19 11:40:00 +0900

    Style: CrossOrigin 애너테이션 클래스 범위로 선언

--- delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/controller/MatchingController.java
+++ delivery-together-backend/src/main/java/com/example/deliverytogetherbackend/controller/MatchingController.java
@@ -9,39 +9,35 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 public class MatchingController {
 
     private final MatchingService matchingService;
 
 
-    @CrossOrigin
     @PostMapping("/matching")
     public ResponseEntity<?> registerMatching(@RequestBody Matching matching) throws Exception {
         return new ResponseEntity<>(matchingService.registerMatching(matching), HttpStatus.CREATED);
     }
 
 
-    @CrossOrigin
     @GetMapping("/matching")
     public ResponseEntity<?> getMatchingList() throws Exception {
         return new ResponseEntity<>(matchingService.getMatchingList(), HttpStatus.OK);
     }
 
 
-    @CrossOrigin
     @GetMapping("/matching/{username}")
     public ResponseEntity<?> getMatchingDetail(@PathVariable String username) throws Exception {
         return new ResponseEntity<>(matchingService.getMatchingDetail(username), HttpStatus.OK);
     }
 
 
-    @CrossOrigin
     @PutMapping("/matching/{username}")
     public ResponseEntity<?> updateMatching(@PathVariable String username, @RequestBody Matching matching) throws Exception {
         return new ResponseEntity<>(matchingService.updateMatching(username, matching), HttpStatus.OK);
     }
 
-    @CrossOrigin
     @DeleteMapping("/matching/{username}")
     public ResponseEntity<?> deleteMatching(@PathVariable String username) {
         return new ResponseEntity<>(matchingService.deleteMatching(username), HttpStatus.OK);
