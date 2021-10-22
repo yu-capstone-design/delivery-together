fcdf0dbbaa39664d4a5628096cd044dae2688f99
임빈영 <qlsdud960604@gmail.com>
2021-10-22 17:04:25 +0900

    Design: 매칭 수정 페이지 UI 변경

--- delivery-together-frontend/src/pages/matching/MatchingUpdateForm.js
+++ delivery-together-frontend/src/pages/matching/MatchingUpdateForm.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Col, Button, Row, Container } from 'react-bootstrap';
+import { Form, Col, Button, Row } from 'react-bootstrap';
 import { readMatchingDetail, updateMatching } from '../../api/matchingService';
 
 const MatchingUpdateForm = (props) => {
@@ -65,106 +65,99 @@ const MatchingUpdateForm = (props) => {
   };
 
   return (
-    <Container>
-      <Form onSubmit={submitMatching}>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'white',
+      }}
+    >
+      <Form
+        style={{
+          width: '40%',
+          padding: 70,
+          borderRadius: '20px',
+          border: '1px solid',
+          borderColor: '#BDBDBD',
+          backgroundColor: '#FAFAFA',
+        }}
+        onSubmit={submitMatching}
+      >
         <br />
         {/* 사용자 이름 */}
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-          <Form.Label column sm={2}>
-            사용자 이름
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              placeholder="사용자 이름을 입력해주세요."
-              onChange={changeValue}
-              name="username"
-              value={matching.username}
-              readOnly={true}
-            />
-          </Col>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="text" onChange={changeValue} name="username" value={matching.username} readOnly={true} />
         </Form.Group>
         {/* 제목 */}
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-          <Form.Label column sm={2}>
-            제목
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              placeholder="제목을 입력해주세요."
-              onChange={changeValue}
-              name="title"
-              value={matching.title}
-            />
-          </Col>
+        <Form.Group className="mb-3" controlId="formHorizontalEmail">
+          <Form.Control
+            type="text"
+            placeholder="제목을 입력해주세요."
+            onChange={changeValue}
+            name="title"
+            value={matching.title}
+          />
         </Form.Group>
         {/* 카테고리 */}
         <fieldset>
           <Form.Group as={Row} className="mb-3" onChange={changeValue}>
-            <Form.Label as="legend" column sm={2}>
-              카테고리
-            </Form.Label>
-
             <Col sm={3}>
               <Form.Check type="radio" label="한식" name="category" value="한식" checked={matching.category === '한식'} />
               <Form.Check type="radio" label="분식" name="category" value="분식" checked={matching.category === '분식'} />
               <Form.Check type="radio" label="디저트" name="category" value="디저트" checked={matching.category === '디저트'} />
             </Col>
+            &nbsp; &nbsp; &nbsp;
             <Col sm={3}>
               <Form.Check type="radio" label="일식" name="category" value="일식" checked={matching.category === '일식'} />
               <Form.Check type="radio" label="치킨" name="category" value="치킨" checked={matching.category === '치킨'} />
               <Form.Check type="radio" label="중식" name="category" value="중식" checked={matching.category === '중식'} />
             </Col>
+            &nbsp; &nbsp; &nbsp;
             <Col sm={3}>
               <Form.Check type="radio" label="피자" name="category" value="피자" checked={matching.category === '피자'} />
               <Form.Check type="radio" label="양식" name="category" value="양식" checked={matching.category === '양식'} />
               <Form.Check type="radio" label="야식" name="category" value="야식" checked={matching.category === '야식'} />
             </Col>
-            <Col sm={10}></Col>
           </Form.Group>
         </fieldset>
         {/* 최대 지불가격 */}
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-          <Form.Label column sm={2}>
-            최대 지불가격
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              placeholder="최대 지불가격을 입력해주세요."
-              onChange={changeValue}
-              name="money"
-              value={matching.money}
-            />
-          </Col>
+        <Form.Group className="mb-3" controlId="formHorizontalPassword">
+          <Form.Control
+            type="text"
+            placeholder="최대 지불가격을 입력해주세요."
+            onChange={changeValue}
+            name="money"
+            value={matching.money}
+          />
         </Form.Group>
         {/* 내용 */}
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-          <Form.Label column sm={2}>
-            내용
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              placeholder="내용을 입력해주세요."
-              onChange={changeValue}
-              name="content"
-              value={matching.content}
-            />
-          </Col>
+        <Form.Group className="mb-3" controlId="formHorizontalEmail">
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="내용을 입력해주세요."
+            onChange={changeValue}
+            name="content"
+            value={matching.content}
+          />
         </Form.Group>
         {/* 버튼 */}
-        <Form.Group as={Row} className="mb-3">
-          <Col sm={{ span: 10, offset: 2 }}>
-            <Button variant="primary" type="submit">
-              매칭 수정
-            </Button>
-          </Col>
-        </Form.Group>
+        <div class="text-center">
+          <Button
+            variant="primary"
+            size="lg"
+            type="submit"
+            style={{ borderRadius: '25px', border: '1px solid', borderColor: '#2E2EFE' }}
+          >
+            매칭 수정
+          </Button>
+        </div>
       </Form>
-    </Container>
+    </div>
   );
 };
 
